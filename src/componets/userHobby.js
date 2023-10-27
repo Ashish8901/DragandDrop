3a29edf4b098b13be071ea0470fc0cc610acaf29
Ashish8901 <ashish8901@gmail.com>
2023-10-27 13:28:25 +0530

    first commit

---
+++ src/componets/userHobby.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const userHobby = () => {
+  return (
+    <div>
+      <h3 style={{ textAlign: 'center' }}>userHobby</h3>
+      <div class="input-group">
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default userHobby;
